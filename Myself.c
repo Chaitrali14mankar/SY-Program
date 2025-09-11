be700e9a8d54ddca622775eb93ff598e13cb8eab
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-09-11 08:05:23 +0530

    Create Myself.c

---
+++ Myself.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("My Name Is Chaitrali Mankar\n");
+    printf("I am in Second Year"\n);
+    printf("I Study In Robotics and Automation Branch\n");
+  
+    return 0;
+}
