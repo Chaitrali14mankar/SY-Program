ccf2228492a00db14a31d05b1b096aa404d49d41
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-09-11 08:07:01 +0530

    Create Cube of number.c

---
+++ Cube of number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    cube = n * n * n;
+  
+    printf("Enter a number: ");
+    scanf("%d", &n);
+  
+    printf("Square = %d\n", cube);
+  
+    return 0;
+}
