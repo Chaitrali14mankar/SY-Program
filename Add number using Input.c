62c9273eaa4fd7caef003f2866080c5a913ca247
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-09-11 07:58:52 +0530

    Create Add number using Input.c

---
+++ Add number using Input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    sum = a + b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+  
+    printf("Sum = %d\n", sum);
+  
+    return 0;
+}
