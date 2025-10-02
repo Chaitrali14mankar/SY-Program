38358fba262fd6bfb665ad8c43247b7e5098e7ab
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 19:01:14 +0530

    Create Pascal’s Triangle (10 rows).c

---
+++ Pascal’s Triangle (10 rows).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int coef;
+    for(int i=0;i<10;i++){
+        coef=1;
+        for(int j=0;j<=i;j++){
+            printf("%d ",coef);
+            coef=coef*(i-j)/(j+1);
+        }
+        printf("\n");
+    }
+}
