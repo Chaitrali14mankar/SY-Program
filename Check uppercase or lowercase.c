27ca8920a157b5afd3ee3871b562bb9a078d915b
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 20:27:09 +0530

    Create  Check uppercase or lowercase.c

---
+++ Check uppercase or lowercase.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char c = 'G';
+    if (c >= 'A' && c <= 'Z') {
+        printf("Uppercase");
+    } else {
+        printf("Lowercase");
+    }
+    return 0;
+}
