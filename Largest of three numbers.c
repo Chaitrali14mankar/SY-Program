392c10e61f0db25b2fc8222c19da33045bf99086
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 20:28:48 +0530

    Create Largest of three numbers.c

---
+++ Largest of three numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 3, b = 9, c = 6;
+    if (a > b && a > c) {
+        printf("%d", a);
+    } else if (b > c) {
+        printf("%d", b);
+    } else {
+        printf("%d", c);
+    }
+    retur
